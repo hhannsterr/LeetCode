6f91a761a9ade8b85e24b01ac0f2204b8c1c16ba
hhannsterr <114409021+hhannsterr@users.noreply.github.com>
2025-05-29 19:06:35 +0700

    Time: 3 ms (88.44%) | Memory: 17.5 MB (99.79%) - LeetSync

--- 9-palindrome-number/palindrome-number.py
+++ 9-palindrome-number/palindrome-number.py
@@ -6,8 +6,5 @@ class Solution:
         x = str(x)
         reversed_x = x[::-1]
         
-        if x == reversed_x:
-            return True
-        else:
-            return False
+        return x == reversed_x
 

4f5207789ac479024a462a16c8629676608b89e0
hhannsterr <114409021+hhannsterr@users.noreply.github.com>
2025-06-23 16:01:52 +0700

    Time: 3 ms (81.20%) | Memory: 18.7 MB (32.98%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -2,7 +2,6 @@ class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
         hash_map = {}
         for i, num in enumerate(numbers):
-            diff = target - num
             if num in hash_map:
                 return [hash_map[num], i+1]
             else:

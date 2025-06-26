18381ffa2ac0187b9d534486f65adf890d0929b3
hhannsterr <114409021+hhannsterr@users.noreply.github.com>
2025-06-26 11:00:41 +0700

    Time: 63 ms (10.57%) | Memory: 18 MB (31.71%) - LeetSync

--- 2395-longest-binary-subsequence-less-than-or-equal-to-k/longest-binary-subsequence-less-than-or-equal-to-k.py
+++ 2395-longest-binary-subsequence-less-than-or-equal-to-k/longest-binary-subsequence-less-than-or-equal-to-k.py
@@ -1,8 +1,19 @@
 class Solution:
     def longestSubsequence(self, s: str, k: int) -> int:
+        def remove_one(s, index):
+            return s[:index] + s[index + 1:]
+        
+        ones = []
+        count = 0
+        for index, letter in enumerate(s):
+            if letter == '1':
+                ones.append(index-count)
+                count += 1
+        ones = ones[::-1]
+
         binary_k = int(bin(k)[2:])
         while True:
             if int(s) > binary_k:
-                s = s.replace('1', '', 1)
+                s = remove_one(s, ones.pop())
             else:
                 return len(s)

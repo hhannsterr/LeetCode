369d567a5ade8e01f31e23066d48ba2eb730c16a
hhannsterr <114409021+hhannsterr@users.noreply.github.com>
2025-07-22 11:48:31 +0700

    Time: 89 ms (99.82%) | Memory: 28.8 MB (50.60%) - LeetSync

--- 1813-maximum-erasure-value/maximum-erasure-value.py
+++ 1813-maximum-erasure-value/maximum-erasure-value.py
@@ -1,19 +1,22 @@
 class Solution:
     def maximumUniqueSubarray(self, nums: List[int]) -> int:
-        output = 0
-        start = 0
-        hash_map = {}
-        for i, num in enumerate(nums):
-            if num in hash_map:
-                if start > hash_map[num]:
-                    hash_map[num] = i
-                else:
-                    output = max(output, sum(nums[start:i]))
-                    start = hash_map[num]+1
-                    hash_map[num] = i
-            else:
-                hash_map[num] = i
+        max_sum = 0
+        current_sum = 0
+        num_set = set()
+        left = 0
 
-        output = max(output, sum(nums[start:]))
-        
-        return output
+        for num in nums:
+            if num in num_set:
+                while nums[left] != num:
+                    current_sum -= nums[left]
+                    num_set.remove(nums[left])
+                    left += 1            
+                
+                left += 1
+            else:
+                num_set.add(num)
+                current_sum += num                    
+                if current_sum > max_sum:
+                    max_sum = current_sum
+
+        return max_sum

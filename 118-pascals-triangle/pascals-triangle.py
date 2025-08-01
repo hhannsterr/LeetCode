fb2d79e44a1b82a912ba043e8ba2a032c71b0544
hhannsterr <114409021+hhannsterr@users.noreply.github.com>
2025-08-02 06:45:22 +0700

    Time: 0 ms (100.00%) | Memory: 17.6 MB (98.13%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.py
+++ 118-pascals-triangle/pascals-triangle.py
@@ -1,19 +1,57 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
         
-        def gen(arr):
+        def gen(num, arr):
             output = [1]
-            length = len(arr)
-            if length >= 2:
-                for i in range(0,length-1):
-                    output.append(arr[i]+arr[i+1])
+            last_arr = arr[-1]
+            for i in range(len(last_arr)-1):
+                output.append(last_arr[i] + last_arr[i+1])
             output.append(1)
             return output
 
-        arr = []
-        output = [[1]]
-        for i in range(1,numRows):
-            arr = gen(arr)
-            output.append(arr)
-        return output
 
+        if numRows == 1:
+            return [[1]]
+        if numRows == 2:
+            return [[1], [1,1]]
+
+        triangle = [[1], [1,1]]
+        for i in range(3, numRows+1):
+            triangle.append(gen(i, triangle))
+
+        return triangle
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # def gen(arr):
+        #     output = [1]
+        #     length = len(arr)
+        #     if length >= 2:
+        #         for i in range(0,length-1):
+        #             output.append(arr[i]+arr[i+1])
+        #     output.append(1)
+        #     return output
+
+        # arr = []
+        # output = [[1]]
+        # for i in range(1,numRows):
+        #     arr = gen(arr)
+        #     output.append(arr)
+        # return output
